e38b0fb65e25d01bea2243dde9f1920e61881eb8
David PHAM-VAN <dev.nfet.net@gmail.com>
2021-10-21 15:07:18 -0300

    Fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,7 @@ import 'pubspec.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- example/lib/pubspec.dart
+++ example/lib/pubspec.dart
@@ -1,59 +1,62 @@
 // This file is generated automatically, do not modify
 
-// ignore_for_file: public_member_api_docs, constant_identifier_names
+// ignore_for_file: public_member_api_docs, constant_identifier_names, avoid_classes_with_only_static_members
 
 class Pubspec {
-  static const String name = 'example';
+  static final buildDate = DateTime.utc(2021, 10, 21, 18, 2, 50);
 
-  static const String description =
-      'Displays an About dialog, which describes the application, can show licenses, changelog, and other infomation.\n';
+  static const name = 'example';
 
-  static const String versionFull = '1.0.0+3';
+  static const description =
+      'Displays an About dialog, which describes the application, can show licenses, changelog, and other information.\n';
 
-  static const String version = '1.0.0';
+  static const versionFull = '1.0.0+3';
 
-  static const String versionSmall = '1.0';
+  static const version = '1.0.0';
 
-  static const int versionMajor = 1;
+  static const versionSmall = '1.0';
 
-  static const int versionMinor = 0;
+  static const versionMajor = 1;
 
-  static const int versionPatch = 0;
+  static const versionMinor = 0;
 
-  static const int versionBuild = 3;
+  static const versionPatch = 0;
 
-  static const String versionPreRelease = '';
+  static const versionBuild = 3;
 
-  static const bool versionIsPreRelease = false;
+  static const String? versionPreRelease = '';
 
-  static const String homepage = 'https://github.com/DavBfr/flutter_about';
+  static const versionIsPreRelease = false;
 
-  static const Map<dynamic, dynamic> environment = <dynamic, dynamic>{
+  static const homepage = 'https://github.com/DavBfr/flutter_about';
+
+  static const environment = <dynamic, dynamic>{
     'sdk': '>=2.1.0 <3.0.0',
   };
 
-  static const Map<dynamic, dynamic> dependencies = <dynamic, dynamic>{
+  static const dependencies = <dynamic, dynamic>{
+    'about': null,
+    'cupertino_icons': null,
     'flutter': <dynamic, dynamic>{
       'sdk': 'flutter',
     },
-    'about': null,
-    'cupertino_icons': null,
   };
 
-  static const Map<dynamic, dynamic> dev_dependencies = <dynamic, dynamic>{
+  static const dev_dependencies = <dynamic, dynamic>{
+    'flutter_lints': '^1.0.0',
     'flutter_test': <dynamic, dynamic>{
       'sdk': 'flutter',
     },
     'pubspec_extract': null,
   };
 
-  static const Map<dynamic, dynamic> dependency_overrides = <dynamic, dynamic>{
+  static const dependency_overrides = <dynamic, dynamic>{
     'about': <dynamic, dynamic>{
       'path': '..',
     },
   };
 
-  static const Map<dynamic, dynamic> flutter = <dynamic, dynamic>{
+  static const flutter = <dynamic, dynamic>{
     'uses-material-design': true,
     'assets': <dynamic>[
       'CHANGELOG.md',
@@ -64,19 +67,19 @@ class Pubspec {
     ],
   };
 
-  static const Map<dynamic, dynamic> builders = <dynamic, dynamic>{
+  static const builders = <dynamic, dynamic>{
     'about': null,
   };
 
-  static const List<String> authors = <String>[
+  static const authors = <String>[
     'David PHAM-VAN <dev.nfet.net@gmail.com>',
   ];
 
-  static const List<String> authorsName = <String>[
+  static const authorsName = <String>[
     'David PHAM-VAN',
   ];
 
-  static const List<String> authorsEmail = <String>[
+  static const authorsEmail = <String>[
     'dev.nfet.net@gmail.com',
   ];
 }
